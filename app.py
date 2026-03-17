2a09b3f31d906fe37f751e40263cf7c1c040df59
fabiox77sousa-netizen <fabiox77sousa@gmail.com>
2026-03-17 09:39:06 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -95,7 +95,10 @@ if total > 0:
     if adicional != "Nenhum":
         msg += f"\nAdicional grátis: {adicional}"
 
-    msg += f"\n\n*Total a pagar: R$ {total_final:.2f}*"
+            msg += f"\n\nSubtotal: R$ {total:.2f}"
+        msg += f"\nTaxa de entrega: R$ 4.00"
+        msg += f"\n*Total a pagar: R$ {total_final:.2f}*"
+
 
     # --- PARTE FINAL CORRIGIDA E ALINHADA ---
     msg_codificada = urllib.parse.quote(msg)
