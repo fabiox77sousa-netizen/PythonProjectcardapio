fa1e6546bb60dd33f842a0548285e520339ecb86
fabiox77sousa-netizen <fabiox77sousa@gmail.com>
2026-03-16 21:32:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -102,4 +102,4 @@ if total > 0:
 else:
     st.sidebar.warning("Seu carrinho está vazio. Escolha um lanche! 😋")
 
-st.markdown("<br><hr><center>Top Burger - Belém/PA</center>", unsafe_allow_html=True)
+st.markdown("<br><hr><center>Top Burger - PA</center>", unsafe_allow_html=True)
